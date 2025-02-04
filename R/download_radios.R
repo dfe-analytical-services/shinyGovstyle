df7963ae2413936c1c4a83a4f2433eec45efaa77
Rich Bielby <richbielby@e.email>
2025-02-04 15:27:33 +0000

    Updated documentation for download_radios

--- R/download_radios.R
+++ R/download_radios.R
@@ -1,11 +1,16 @@
 #' Download with extension radios
 #'
-#' @param id
-#' @param download_type
-#' @param file_types
-#' @param file_sizes
+#' @param id Shiny element ID. Default is "download_radios", but must be customised to be unique
+#' if multiple instances of this module are being used in a single app. Must match up to the ID
+#' of a `download_radios_helper()` instance in server code.
+#' @param download_type Element being downloaded. Expected to be along the lines of "underlying
+#' data", "table", "current data view". Default is "table".
+#' @param file_types File formats to offer, can be any combination of "CSV", "XLSX", "ODS". Default
+#' is a vector of all 3.
+#' @param file_sizes Estimated file sizes for each file format. This needs to be a vector of the
+#' same length as file_types.
 #'
-#' @returns
+#' @returns UI containing radio selection and download button
 #' @export
 #'
 #' @examples
@@ -13,12 +18,21 @@ download_radios <- function(
     id = "download_radios",
     download_type = "table",
     file_types = c("CSV", "ODS", "XLSX"),
-    file_sizes = c("< 1 GB", "< 1 GB", "< 1 GB")
-    ){
+    file_sizes = c("< 1 GB", "< 1 GB", "< 1 GB")) {
+  allowed_extensions <- c("CSV", "ODS", "XLSX")
+  if (is.null(file_types)) {
+    file_types <- allowed_extensions
+  }
+  if (any(!(toupper(file_types) %in% allowed_extensions))) {
+    stop(
+      "File types must one of \"CSV\", \"ODS\", \"XLSX\".\nValue provided: ",
+      setdiff(file_types, allowed_extensions) |> paste(collapse = ", ")
+    )
+  }
   shiny::tagList(
     radio_button_Input(
       inputId = shiny::NS(id, "file_extension"),
-      choices = paste0(file_types," (",  file_sizes, ")"),
+      choices = paste0(file_types, " (", file_sizes, ")"),
       selected = file_types[1],
       label = "Select file format:"
     ),
@@ -37,9 +51,11 @@ download_radios <- function(
 
 #' Download with extension radios handler
 #'
-#' @param id
-#' @param file_name
-#' @param file_contents
+#' @param id Shiny element ID. Default is "download_radios", but must be customised to be unique
+#' if multiple instances of this module are being used in a single app. Must match up to the ID
+#' of a `download_radios()` instance in UI code.
+#' @param file_name Name of the file to be downloaded
+#' @param file_contents Contents to write to the download file
 #'
 #' @returns
 #' @export
@@ -73,3 +89,4 @@ download_radios_handler <- function(
     }
   )
 }
+#

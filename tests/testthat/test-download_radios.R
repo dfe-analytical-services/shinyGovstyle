3238dc2375dc5d7f4f2fec6c7b434b50cc376579
Rich Bielby <richbielby@e.email>
2025-02-12 12:57:50 +0000

    Added small bits of testing for downloads_radios

--- tests/testthat/test-download_radios.R
+++ tests/testthat/test-download_radios.R
@@ -17,4 +17,10 @@ test_that("Suffixes present", {
       stringr::str_detect(c("XLSX")) |>
       isFALSE()
   )
+  expect_error(
+    download_radios_handler(file_name = c("sdf", "sdfsdgf"), file_contents = data.frame(z=c(1)))
+  )
+  expect_error(
+    download_radios_handler(file_name = "sdfsdf", file_contents = 1)
+  )
 })

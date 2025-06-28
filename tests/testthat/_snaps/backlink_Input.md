bd94a6efe9cb48e7daa5e4cb0fdf511fddedeaf5
cjrace <52536248+cjrace@users.noreply.github.com>
2025-06-28 07:25:34 +0100

    add snapshot onto backlink test

---
+++ tests/testthat/_snaps/backlink_Input.md
@@ -0,0 +1,7 @@
+# backlink HTML is as expected
+
+    Code
+      backlink_Input("backId")
+    Output
+      <a class="action-button govuk-back-link" href="#" id="backId">Back</a>
+
--- tests/testthat/test-backlink_Input.R
+++ tests/testthat/test-backlink_Input.R
@@ -1,3 +1,8 @@
 test_that("backlink works", {
   expect_no_error(backlink_Input("backId"))
 })
+
+test_that("backlink HTML is as expected", {
+  local_edition(3)
+  expect_snapshot(backlink_Input("backId"))
+})

79a9da35e8231f7ee5de590c04da99dea252a8b2
Sarah Brown <75032474+sarahmwong@users.noreply.github.com>
2025-07-10 11:24:58 +0100

    150 table functions dont allow reactive tables (#153)
    * get pair of output/render functions working with interactive gov table - need to fix caption sizing
    
    * basic working with interactive -falls over with non reactive still need to fix caption handling between both options (testing if else logic)
    
    * adding test script for jeni
    
    * update to remove caption from static function and stick in output instead. Lots of faffing to get the example to actually work! Just needed to load in dplyr...
    
    * use gov select instead
    
    * remove test file
    
    * original tests to pass (change from expecting html to expect reactable)
    
    * add documentation for output and render functions - follows pattern of reactable documentation
    
    * WIP testing shortening names
    
    * add heading_level argument to govReactable
    
    * merge in CSS and custom formatting from DfE example
    
    * updated docs and tests
    
    * Prevent column resizing, update testing
    
    * add ability to customise col widths
    
    * fix typo
    
    * tidy up after merging in latest
    
    * remove the right_class argument - doesn't do anything now align fixes this
    
    ---------
    
    Co-authored-by: Cam Race <52536248+cjrace@users.noreply.github.com>

---
+++ R/govReactable.R
@@ -0,0 +1,200 @@
+#' Interactive govTable
+#'
+#' This function inserts a government-styled table using `reactable`.
+#' You can use this in R markdown or Quarto documents, or use
+#' renderGovReactable({}) and govReactableOutput() for tables in R Shiny.
+#' govReactableOutput() gives the ability to add a caption, for static
+#' tables made using just govReactable(), use heading_text() to add
+#' captions to tables.
+#'
+#' @description
+#' This function is opinionated and sets table defaults that are in
+#' keeping with the wider GOV.UK design system. Some defaults are overrideable,
+#' such as `highlight=TRUE` and `borderless=TRUE`, however some are fixed, such
+#' as `showSortIcon=FALSE` as the default sort icon is inaccessible. Additional
+#' arguments from `reactable::reactable` can be passed to customise the table.
+#'
+#' @param df A dataframe used to generate the table.
+#' @param right_col A vector of column names that should be right-aligned.
+#' By default, numeric data is right-aligned, and character data is
+#' left-aligned.
+#' @param page_size The default number of rows displayed per page (default: 10).
+#' @param highlight Highlight table rows on hover.
+#' @param borderless Remove inner borders from table.
+#' @param min_widths Customise minimum column width using a list of columns and
+#' minimum width in pixels.
+#' @param ... Additional arguments passed to `reactable::reactable`.
+#' @return A `reactable` HTML widget styled with GOV.UK classes.
+#' @keywords table, reactable, GOV.UK
+#' @export
+#' @examples
+#' # Example static table using govReactable
+#' if (interactive()) {
+#'   govReactable(
+#'     iris,
+#'     right_col = c(
+#'       "Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"
+#'     )
+#'   )
+#'
+#'   govReactable(
+#'     iris,
+#'     right_col = c(
+#'       "Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"
+#'     ),
+#'     highlight = FALSE,
+#'     page_size = 5,
+#'     min_widths = list(
+#'       Sepal.Length = 75,
+#'       Sepal.Width = 75,
+#'       Petal.Length = 75,
+#'       Petal.Width = 75
+#'     )
+#'   )
+#' }
+govReactable <- function(
+  df,
+  right_col = NULL,
+  page_size = 10,
+  highlight = TRUE,
+  borderless = TRUE,
+  min_widths = list(),
+  ...
+) {
+  # Generate column definitions
+  col_defs <- stats::setNames(
+    lapply(seq_along(names(df)), function(index) {
+      col <- names(df)[index]
+
+      reactable::colDef(
+        name = col,
+        sortable = TRUE,
+        headerClass = "govuk-table__header",
+        class = "govuk-table__cell",
+        html = TRUE,
+        na = "NA",
+        align = if (!is.null(right_col) && col %in% right_col) {
+          "right"
+        } else {
+          "left"
+        },
+        minWidth = if (!is.null(min_widths[[col]])) min_widths[[col]] else NULL
+      )
+    }),
+    names(df)
+  )
+
+  # Create the reactable table
+  table <- reactable::reactable(
+    df,
+    columns = col_defs,
+    defaultPageSize = page_size,
+    highlight = highlight,
+    borderless = borderless,
+    showSortIcon = FALSE,
+    defaultColDef = reactable::colDef(headerClass = "bar-sort-header"),
+    fullWidth = TRUE,
+    wrap = TRUE,
+    rowClass = "govuk-table__row",
+    class = "gov-table govuk-table",
+    ...
+  )
+
+  # Attach the govReactable CSS dependency
+  # TODO: Fix, as this is not pulling through
+  version <- as.character(packageVersion("shinyGovstyle")[[1]])
+
+  dependency <- htmltools::htmlDependency(
+    name = "stylecss",
+    version = version,
+    src = c(href = "shinyGovstyle/css"),
+    stylesheet = "govReactable.css"
+  )
+
+  table <- htmltools::attachDependencies(table, dependency, append = TRUE)
+  return(table)
+}
+
+#' Shiny bindings for govReactable
+#' Output and render functions for using govReactable within shiny apps
+#'
+#' @param output_table_name Output variable to read from.
+#' @param caption Adds a caption to the table as a header.
+#' @param caption_size Adjust the size of caption.
+#' Options are s, m, l, xl, with l as the default.
+#' @param heading_level The HTML heading level for
+#' the caption (e.g., "h2", "h3", "h4", "h5"). Default is "h2".
+#' @param expr An expression that generates a `reactable` widget.
+#' @param env The environment in which to evaluate `expr`.
+#' @param quoted Is `expr` a quoted expression (with [quote()])?
+#' This is useful if you want to save an expression in a variable.
+#' @return `govReactableOutput()` returns a `reactable` output element
+#' that can be included in a Shiny UI.
+#'
+#' `renderGovReactable()` returns a `reactable` render function that
+#' can be assigned to a Shiny output slot.
+#'
+#' @name govReactable-shiny
+#'
+#' @examples
+#' # Run in an interactive R session
+#' if (interactive()) {
+#'   library(shiny)
+#'   library(shinyGovstyle)
+#'
+#'   ui <- fluidPage(
+#'     titlePanel("govReactableOutput example"),
+#'     govReactableOutput("table")
+#'   )
+#'
+#'   server <- function(input, output, session) {
+#'     output$table <- renderGovReactable({
+#'       govReactable(iris)
+#'     })
+#'   }
+#'
+#'   shinyApp(ui, server)
+#' }
+#'
+#' @export
+# Output for reactive tables
+
+govReactableOutput <- function(
+  output_table_name,
+  caption,
+  caption_size = "l",
+  heading_level = "h2"
+) {
+  # Validate heading_level input
+  allowed_levels <- c("h2", "h3", "h4", "h5")
+  if (!heading_level %in% allowed_levels) {
+    stop(
+      "heading_level must be one of: ",
+      paste(allowed_levels, collapse = ", ")
+    )
+  }
+
+  heading_tag <- do.call(
+    shiny::tags[[heading_level]],
+    list(
+      class = paste0("govuk-heading-", caption_size),
+      caption
+    )
+  )
+
+  return(htmltools::div(
+    heading_tag,
+    reactable::reactableOutput(output_table_name)
+  ))
+}
+
+# use renderReactable to render the govTables - naming just for convention
+# This function wraps reactable::renderReactable.
+#' @rdname govReactable-shiny
+#' @export
+renderGovReactable <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) {
+    expr <- substitute(expr)
+  }
+  reactable::renderReactable(expr, env = env, quoted = TRUE)
+}
---
+++ tests/testthat/test-govReactable.R
@@ -0,0 +1,28 @@
+test_that("table works", {
+  # Sample data
+  Months <- rep(c("January", "February", "March", "April", "May"), times = 2)
+  Colours <- rep(c("Red", "Blue"), times = 5)
+  Bikes <- c(85, 75, 165, 90, 80, 95, 85, 175, 100, 95)
+  Cars <- c(95, 55, 125, 110, 70, 120, 60, 130, 115, 90)
+  Vans <- c(150, 130, 180, 160, 140, 175, 135, 185, 155, 145)
+  Buses <- c(200, 180, 220, 210, 190, 215, 185, 225, 205, 195)
+  example_data <- data.frame(Months, Colours, Bikes, Cars, Vans, Buses)
+
+  # Test table with sorting and pagination enabled
+  table_check <- govReactable(
+    df = example_data,
+    right_col = c("Colours", "Bikes", "Cars", "Vans", "Buses"),
+    page_size = 5
+  )
+
+  # Ensure the function runs without errors
+  expect_no_error(table_check)
+
+  # Take snapshot of table HTML
+  output_html <- htmltools::renderTags(table_check)$html
+
+  # Prevent unnecessary changes due to random IDs
+  stripped_ids <- gsub('"htmlwidget-[^"]*"', '', output_html)
+
+  expect_snapshot(stripped_ids)
+})

b38a59cf92b8e4b94abcb96c80c4e4877d1df238
Cam Race <52536248+cjrace@users.noreply.github.com>
2025-10-07 19:12:24 +0100

    update skip to main

--- tests/testthat/test-skip_to_main.R
+++ tests/testthat/test-skip_to_main.R
@@ -1,3 +1,4 @@
 test_that("test skip_to_main_href", {
-  expect_equal(skip_to_main()$attribs$href, "#main_col")
+  expect_equal(skip_to_main()$attribs$href, "#main")
+  expect_equal(skip_to_main("main_col")$attribs$href, "#main_col")
 })
